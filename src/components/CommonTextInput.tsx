6d373c4259805397d5114f24b77693cd1f92857d
Dipanshu Jindal <>
2025-01-19 01:55:18 -0500

    saferarea vie addedd

---
+++ src/components/CommonTextInput.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  View,
+  TextInput,
+  TextInputProps,
+  StyleSheet,
+  Image,
+  ImageSourcePropType,
+} from 'react-native';
+
+interface CommonTextInputProps extends TextInputProps {
+  isSvg?:boolean;
+  iconSource: ImageSourcePropType; // Source of the icon image
+  iconSize?: number; // Size of the icon (width and height will be the same)
+}
+
+const CommonTextInput: React.FC<CommonTextInputProps> = ({
+  isSvg=false,
+  iconSource,
+  iconSize = 24,
+  style,
+  ...textInputProps
+}) => {
+  // Needs to pass image also from previous screen
+  return (
+    <View style={styles.container}>
+      {iconSource &&
+          <View style={styles.icon}>
+            {isSvg? iconSource: <Image
+                source={iconSource}
+                style={[{width: iconSize, height: iconSize}]}
+            />}
+          </View>
+      }
+      <TextInput
+        style={[styles.input, style]}
+        placeholderTextColor={'white'}
+        {...textInputProps}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#E4DFDF',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: 'white',
+  },
+});
+
+export default CommonTextInput;
